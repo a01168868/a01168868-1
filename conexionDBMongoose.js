c0736fce28cd49bfd5e38afa02709f6694a43855
Miguel Torres <mtc590@gmail.com>
2021-10-12 16:59:27 -0500

    CRUD con Mongoose

--- conexionDBMongoose.js
+++ conexionDBMongoose.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 
 //mongoose.connect('mongodb://vmanager:root@3.94.165.232:27017/baseX?authSource=tecCEM')
-mongoose.connect('mongodb://vmanager:root@54.173.202.133:27017/testdb?authSource=tecCEM')
+mongoose.connect('mongodb://vmanager:tecCEM@54.173.202.133:27017/testdb?authSource=admin')
 .then(()=>console.log("conexion exitosa"))
 .catch(err=>console.log(err))
 
@@ -12,27 +12,66 @@ const PokemonSchema=mongoose.Schema({
         type:String,
         required:true
     },
+    numero:{
+        type:Number
+    },
+    tipo:{
+        type:String
+    },
     descripcion:{
         type: String,
         required: false
     }
-})
+},{collection:'pokemon'});
 
 //Crear modelo
 const Pokemon=mongoose.model('Pokemon',PokemonSchema);
 
 //crea una instancia del modelo
 const pokemon=new Pokemon({
-    nombre: "Charizard",
-    descripcion: "Pokemon superMegaPoderoso de fuego",
+    nombre: "Snorlax",
+    descripcion: "Está gordo y duerme mucho, como yo",
+    tipo: "normal",
+    numero: 5,
     _id: new mongoose.Types.ObjectId()
 })
 
 //Guarda la instancia como documento dentro de la colección 
 //especificada por el modelo
-pokemon.save()
+
+//CREATE
+/*pokemon.save()
     .then(()=>{
         console.log(pokemon)
         console.log("Pokemon creado")
     })
     .catch(err=>console.log(err))
+    */
+
+    //READ
+    Pokemon.find()
+    .then(resultado=>{
+        console.log(resultado[1].nombre);
+    })
+    .catch()
+
+    Pokemon.findOne()
+    .then(resultado=>{
+        console.log(resultado.nombre);
+    })
+    .catch()
+
+    //UPDATE
+    async function actualizar(){
+        try{
+            await Pokemon.findOneAndUpdate({nombre:"Snorlax"},{numero:666});
+            let pok=await Pokemon.findOne({nombre:"Snorlax"});
+            console.log(pok);
+        }catch(err){
+            console.log(err);
+        }
+    }
+
+    actualizar();
+
+    //DELETE  findOneAndRemove...
